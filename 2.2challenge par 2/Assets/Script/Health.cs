519682bbc53dbb2b281f94a12dc4c317429cc714
Mohamud <amohamud@cherrycreekschools.org>
2022-10-11 10:08:36 -0600

    Update Health.cs

--- 2.2challenge par 2/Assets/Script/Health.cs
+++ 2.2challenge par 2/Assets/Script/Health.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-
+    int heart = 100;
     // Start is called before the first frame update
     void Start()
     {
-        heart = 100;
     }
 
     // Update is called once per frame
