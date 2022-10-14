ee16fac9dc2295f0f7617aab9829b9ab2395d7de
Mohamud <amohamud@cherrycreekschools.org>
2022-10-14 10:29:40 -0600

    Challenge 2.4
    we made our player not go through the ground

---
+++ 2.4Challenge/Assets/new/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int player = 4;
+    Rigidbody2D man = null;
+    void Start()
+    {
+        Debug.Log(player);
+        Debug.Log(man);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
